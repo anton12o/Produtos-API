2e4e78950af98e21843f13def9151fe613468689
anton12o <antoniofrota334@gmail.com>
2026-03-28 22:33:44 -0300

    fix env railway

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
